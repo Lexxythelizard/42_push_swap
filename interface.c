cadacc456f5ec41de8cf15df389e28657ac94d12
Lenina Vorbau <lenivorb@c3a7c1.42berlin.de>
2026-05-26 17:37:56 +0200

    solved 1/3 todo in interface.c, todo 1 unclear, see comments...

--- interface.c
+++ interface.c
@@ -6,7 +6,7 @@
 /*   By: rcollet <rcollet@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/26 12:37:13 by rcollet           #+#    #+#             */
-/*   Updated: 2026/05/26 13:03:08 by rcollet          ###   ########.fr       */
+/*   Updated: 2026/05/26 17:35:59 by lenivorb         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,6 +28,59 @@ t_interface	*init(int argc, char **argv)
 	free(nums);
 }
 
-/* TODO: Check for duplicate values, check for invalid (non-numeric + '-') inputs,
-   implement utility functions for the interface operations (see exercise), 
-   there are already s, p, r and rr functions that work on stacks in stack.c */
+/* TODO: 
+	- Check for duplicate values								: unclear 
+	- check for invalid (non-numeric + '-') inputs 				: DONE
+    - implement utility functions for the interface operations 	: on it
+	  (see exercise), there are already s, p, r and rr 
+	  functions that work on stacks in stack.c 
+
+*/
+
+// === Check for duplicate values: unclear what you want me todo.... ===
+
+// 	chould check if there are double values
+int	lxy_has_duplicate_values(/*stack*/)
+{
+	//	--> O(n^2) or O(log(n)*n) with two pointers if my math is right
+	// return 1 / 0
+}
+
+// 	chould it check if there are at least one double values in the list
+int	lxy_is_doupled_values(t_stack_frame *node)
+{
+	//	--> O(n)
+	// return 1 / 0
+}
+
+// 	chould check if there are double values
+t_stack_frame	*lxy_get_duplicate_value(t_stack_frame *node)
+{
+	//	--> O(n)
+	// returns a pointer to the node that has the same value or the node itself 
+}
+
+// === check for invalid (non-numeric + '-') inputs ===
+
+/*
+lxy_is_nbr_input_valid
+	- Nullpointer guard
+	- skipping whitespaces
+	- skipping single sign
+	- return is_numeric
+
+	--> return 0 / 1
+*/
+
+int lxy_is_nbr_input_valid(const char *s)
+{
+	size_t	i;
+
+	i = 0;
+	if (!(s))
+		return (0);
+	while((s[i]) && (((s[i] >= 9) && (s[i] <= 13)) || (s[i] == 32)))
+		i++;
+	i += ((s[i] == 43) || (s[i] == 45));
+	return (ft_isnumeric(s[i]));
+}

f6b7153683baeee16f527ee80f0ac34a4a785157
Lenina Vorbau <lenivorb@c3a7c1.42berlin.de>
2026-05-29 21:52:43 +0200

    modified interface.c slightly (sry last commit message was wrong)

--- interface.c
+++ interface.c
@@ -6,12 +6,33 @@
 /*   By: rcollet <rcollet@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/26 12:37:13 by rcollet           #+#    #+#             */
-/*   Updated: 2026/05/29 15:12:13 by lenivorb         ###   ########.fr       */
+/*   Updated: 2026/05/29 19:53:11 by lenivorb         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 /* Initialises the interface, containing a filled stack a from argv and an
    empty stack b */
+
+t_interface	*init(int argc, char **argv)
+{
+	int			*nums;
+	t_interface	*rtrn;
+	
+	rtrn = malloc(16);
+	if (!rtrn)
+		return (NULL);
+	nums = get_int_list(argv);
+	if (!nums)
+		return (NULL);
+	rtrn -> a = stack_init(nums, argc - 1);
+	rtrn -> b = stack_init(NULL, 0);
+	free(nums);
+	return (rtrn);
+}
+
+//	--- ravy original ---
+
+/*
 t_interface	*init(int argc, char **argv)
 {
 	int			*nums;
@@ -32,14 +53,4 @@ t_interface	*init(int argc, char **argv)
 	free(nums);
 	return (rtrn);
 }
-
-/* TODO: 
-	- Check for duplicate values (in argv!)						: DONE (4 function bc unclear)
-																  moved in file: 
-	- check for invalid (non-numeric + '-') inputs 				: DONE
-    - implement utility functions for the interface operations 	: on it
-	  (see exercise), there are already s, p, r and rr 
-	  functions that work on stacks in stack.c 
-
 */
-

68f84c994eac677827e7ec7daf5be83d09888806
Lenina Vorbau <lenivorb@c3a6c6.42berlin.de>
2026-06-03 14:10:00 +0200

    refactored validation: is_args_valid() : see comment

--- validation.c
+++ validation.c
@@ -6,7 +6,7 @@
 /*   By: lenivorb <lenivorb@student.42berlin.d      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/29 15:07:03 by lenivorb          #+#    #+#             */
-/*   Updated: 2026/05/29 17:14:15 by lenivorb         ###   ########.fr       */
+/*   Updated: 2026/06/03 14:09:17 by lenivorb         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,22 +20,43 @@
 
 // --- define ---
 
+/*
+refactored: now:								24 lines :)
+	- works with flag counter
+	- if iteration neither flag nor nbr 		--> 0
+	- flags are just allowed as first two args	--> 0
+	- if 2 flags and both are not --bench 		-->	0
+	- if both flags are identicall				--> 0
+	- if there are mor then two flags			--> 0
+*/
+
 int	is_args_valid(const char **argv)
 {
-	size_t	i;
+	int	i;
+	int	flags;
 
-	i = 0;
+	i = (-1);
+	flags = 0;
 	if (!(argv))
 		return (0);
-	while (argv[i] != NULL)
+	while (argv[i++] != NULL)
 	{
-		if ((!(is_valid_nbr(argv[i]))) || (!(is_valid_flag(argv[i]))))
+		if ((!(is_valid_nbr(argv[i]))) && (!(is_valid_flag(argv[i]))))
 			return (0);
-		i++;
+		if (is_valid_flag(argv[i]))
+			flags++;
 	}
-	return (1);
+	if ((flags == 1) && (!(is_valid_flag(argv[0]))))
+		return (0);
+	if ((flag == 2) && (!((is_valid_flag(argv[0])) && (is_valid_flag(argv[1])))))
+		return (0);
+	if ((flag == 2) && (ft_strncmp(argv[0], FLAG_BENCH, 11)) && 
+		(ft_strncmp(argv[1], FLAG_BENCH, 11)))
+		return (0);
+	if ((flag == 2) && (!(ft_strncmp(argv[0], argv[1], 11))))
+		return (0);
+	return (flags <= 2);
 }
-
 
 /*
 is_valid_nbr

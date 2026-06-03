350cc286322ae0bfae2ba3ac425f39b621dbb9a4
Lenina Vorbau <lenivorb@c3a6c6.42berlin.de>
2026-06-03 18:06:54 +0200

    never mind no dead end, actually pseudocoded merge utility: need two more coordinating functions... see sort_complex.c

--- sort_complex.c
+++ sort_complex.c
@@ -6,7 +6,7 @@
 /*   By: lenivorb <lenivorb@student.42berlin.d      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/03 15:27:02 by lenivorb          #+#    #+#             */
-/*   Updated: 2026/06/03 16:20:28 by lenivorb         ###   ########.fr       */
+/*   Updated: 2026/06/03 18:05:09 by lenivorb         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -48,18 +48,41 @@ void	func0(void)
 
 // --- utilities ---
 
-/*	...you comment... */
+/*ms: merge size: max size of the resulting stack: allways even */
 
-void	merge(t_stack dest, t_stack sec, int ms)
+/*
+	 merge deeps in dest (stack b) [ms / 2]
+	 merge deeps in sec  (stack a) <= [ms / 2]
+	 keep track of the rotatios and ([new]: not) rotate back everytime
+
+	 new estimation:
+		 worst case !simplified!:	rotations 2 * (ms / 2) + ms
+									pushes ms / 2
+
+		--> O(n)	for merge yeah I was an Idiot^^
+*/
+
+void	merge(t_stack *dest, t_stack *sec, int ms)
 {
-	// merge deeps in dest (stack b) [ms / 2]
-	// merge deeps in sec  (stack a) <= [ms / 2]
-	// keep track of the rotatios and rotate back everytime
-	// worst case !simplified! : (ms / 2 + ms) * (ms / 4) * 2
 
-	//	 --> ms^2 rotations --> O(n) for merge....
+	int	pos;
+	int	i;
+	int	left;
 
-	// too much .... can't do ....
+	pos = 0;
+	i = 0;
+	left = (ms / 2)
+	while ((left--) && (sec -> head))
+	{
+		while (i++ <= (ms / 2) && (sec -> head -> val < dest -> head -> val))
+		{
+			// exec rb 1 time
+			pos++;
+		}
+		// exec pb 1 time;
+	}
+	while (pos--)
+		//exec rrb 1 time
 }
 
 /*	...you comment... */

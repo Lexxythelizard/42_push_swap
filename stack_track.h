2a3503405694428fbddbfe5fc94307b74a1f5e99
Lenina Vorbau <lenivorb@c4a8c7.42berlin.de>
2026-06-04 16:56:08 +0200

    oh forgot to push this struct

---
+++ stack_track.h
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   stack_track.h                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lenivorb <lenivorb@student.42berlin.d      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/06/04 16:17:20 by lenivorb          #+#    #+#             */
+/*   Updated: 2026/06/04 16:55:34 by lenivorb         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef STACK_TRACK_H
+# define STACK_TRACK_H
+
+// --- include ---
+
+// --- DOC ---
+
+/*
+	...you comment here...
+*/
+
+// --- Makros ---
+
+# define YOUR_DEFVAL 161
+
+// --- struct ---
+
+typedef struct s_stack_track
+{
+	int		size_a;
+	int		size_b;
+	int		merge_size;
+	int		side;
+}			t_stack_track;
+
+// --- end ---
+
+#endif
+

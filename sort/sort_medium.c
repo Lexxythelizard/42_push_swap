ca44764729d33c6faf5933a472ad1612f78cc9cc
Lexxythelizard <Lenina.vorbau@posteo.de>
2026-07-23 10:21:24 +0200

    wrote functions in -/sort/sort_medium.c more norminette conform, still need to source out things

--- sort/sort_medium.c
+++ sort/sort_medium.c
@@ -5,130 +5,217 @@
 // test
 #include <stdio.h>
 
-float my_sqrt(int x) {
-	if (x < 0) return -1;
-	if (x == 0) return 0;
+float	my_sqrt(int x)
+{
+	double	guess;
+	int		i;
 
-	double guess = x;
-
-	int i = 0;
-	while (i < 30) {
+	if (x < 0)
+		return (-1);
+	if (x == 0)
+		return (0);
+	guess = x;
+	i = 0;
+	while (i < 30)
+	{
 		guess = (guess + x / guess) / 2.0;
 		i++;
 	}
-
-	return guess;
+	return (guess);
 }
 
-int getMax(t_stack *stack) {
-	int max = stack->first->val;
-	t_element* currentElement = stack->first->next;
+int	getMax(t_stack *stack)
+{
+	int			max;
+	t_element	*currentElement;
+
+	max = stack -> first -> val;
+	currentElement = stack -> first -> next;
 	while (currentElement) {
-		if (currentElement->val > max) max = currentElement->val;
+		if (currentElement -> val > max)
+			max = currentElement -> val;
+		currentElement = currentElement -> next;
+	}
+	return (max);
+}
+
+int	getMin(t_stack* stack)
+{
+	int			min;
+	t_element	*currentElement;
+
+	min = stack -> first -> val;
+	currentElement = stack -> first -> next;
+	while (currentElement)
+	{
+		if (currentElement->val < min)
+			min = currentElement->val;
 		currentElement = currentElement->next;
 	}
-	return max;
+	return (min);
 }
 
-int getMin(t_stack* stack) {
-	int min = stack->first->val;
-	t_element* currentElement = stack->first->next;
-	while (currentElement) {
-		if (currentElement->val < min) min = currentElement->val;
-		currentElement = currentElement->next;
-	}
-	return min;
-}
+static int	isElementInBucket(
+				t_element *element,
+				t_element *bucket[],
+				int count)
+{
+	int	i;
 
-static int isElementInBucket(t_element* element, t_element* bucket[], int count) {
-	int i = 0;
-	while (i < count) {
+	i = 0;
+	while (i < count)
+		{
 		if (bucket[i] == element)
-			return 1;
+			return (1);
 		i++;
 	}
-	return 0;
+	return (0);
 }
 
-void addBucketToStackB(t_element* bucket[], int count, t_stack_machine* machine) {
-	t_stack* stack = &machine->stacks[0];
-	int remaining = count;
-	while (remaining > 0) {
-		if (isElementInBucket(stack->first, bucket, count)) {
+void	addBucketToStackB(
+			t_element *bucket[],
+			int count,
+			t_stack_machine *machine)
+{
+	t_stack	*stack;
+	int		remaining;
+
+	stack = &(machine -> stacks[0]);
+	remaining = count;
+	while (remaining > 0)
+	{
+		if (isElementInBucket(stack->first, bucket, count))
+		{
 			machine_operation_execute(machine, PB);
 			remaining--;
-		} else {
+		}
+		else
+		{
 			machine_operation_execute(machine, RA);
 		}
 	}
 }
 
-void addAllBucketsToStackB(int numberOfBuckets, int len,
-	t_element* buckets[numberOfBuckets][len], int* numberOfElements,
-	t_stack_machine* machine) {
-	int execution = numberOfBuckets - 1;
-	while (execution >= 0) {
-		addBucketToStackB(buckets[execution], numberOfElements[execution], machine);
+void	addAllBucketsToStackB(
+			int numberOfBuckets,
+			int len,
+			t_element* buckets[numberOfBuckets][len],
+			int* numberOfElements,
+			t_stack_machine* machine)
+{
+	int	execution;
+
+	execution = numberOfBuckets - 1;
+	while (execution >= 0)
+	{
+		addBucketToStackB(
+			buckets[execution],
+			numberOfElements[execution],
+			machine);
 		execution--;
 	}
 }
 
-void createBuckets(t_stack* aStack, int numberOfBuckets, long range, int min,
-	int len, t_element* buckets[numberOfBuckets][len], int* numberOfElements) {
-	t_element* currentElement = aStack -> first;
-	while (currentElement) {
-		long normalizedVal = (long)currentElement -> val - (long)min;
-		int bucketNumber = (int)((normalizedVal * numberOfBuckets) / range);
+void	createBuckets(
+			t_stack* aStack,
+			int numberOfBuckets,
+			long range,
+			int min,
+			int len,
+			t_element *buckets[numberOfBuckets][len],
+			int* numberOfElements)
+{
+	t_element	*currentElement;
+	long		normalizedVal;
+	int			bucketNumber;
+
+	currentElement = aStack -> first;
+	while (currentElement)
+	{
+		normalizedVal = (long)currentElement -> val - (long)min;
+		bucketNumber = (int)((normalizedVal * numberOfBuckets) / range);
 		buckets[bucketNumber][numberOfElements[bucketNumber]] = currentElement;
 		numberOfElements[bucketNumber]++;
 		currentElement = currentElement -> next;
 	}
 }
 
-void sortStack(t_stack* stack) {
-	if (!stack || stack->len <= 1)
+void	sortStack(
+			t_stack* stack)
+{
+	t_element	*current;
+	t_element	*node;
+	t_element	*prev;
+	int			tmp;
+
+	if ((!stack) || (stack -> len <= 1))
 		return;
-	t_element* current = stack->first->next;
-	while (current) {
-		t_element* node = current;
-		t_element* prev = node->prev;
-		while (prev && prev->val < node->val) {
-			int tmp = prev->val;
-			prev->val = node->val;
-			node->val = tmp;
+	current = stack -> first -> next;
+	while (current)
+	{
+		node = current;
+		prev = node -> prev;
+
+		while (prev && prev -> val < node -> val)
+		{
+			tmp = prev -> val;
+			prev -> val = node -> val;
+			node -> val = tmp;
 			node = prev;
-			prev = node->prev;
+			prev = node -> prev;
 		}
-		current = current->next;
+		current = current -> next;
 	}
 }
 
-void pushBtoA(t_stack_machine* machine) {
+void pushBtoA(t_stack_machine *machine)
+{
 	if (!machine)
-		return;
+		return ;
 	while (machine->stacks[1].len > 0)
 		machine_operation_execute(machine, PA);
 }
 
-void sort_medium(t_stack_machine* machine) {
-	t_stack aStack = machine -> stacks[0];
-	int max = getMax(&aStack);
-	int min = getMin(&aStack);
-	long range = (long)max - (long)min + 1;
-	int numberOfBuckets = my_sqrt(aStack.len);
+void sort_medium(t_stack_machine *machine)
+{
+	t_stack		aStack;
+	t_element	*cursor;
+	int			max;
+	int			min;
+	long		range;
+	int			numberOfBuckets;
+	int			*numberOfElements;
 
-	int *numberOfElements = calloc(numberOfBuckets, sizeof(int));
+	aStack = machine -> stacks[0];
+	max = getMax(&aStack);
+	min = getMin(&aStack);
+	range = (long)(max) - (long)(min + 1);
+	numberOfBuckets = my_sqrt(aStack.len);
+	numberOfElements = calloc(numberOfBuckets, sizeof(int));
 	t_element* buckets[numberOfBuckets][aStack.len];
 
-	createBuckets(&aStack, numberOfBuckets, range, min, aStack.len, buckets, numberOfElements);
+	createBuckets(
+		&aStack,
+		numberOfBuckets,
+		range,
+		min,
+		aStack.len,
+		buckets,
+		numberOfElements);
 
-	addAllBucketsToStackB(numberOfBuckets, aStack.len, buckets, numberOfElements, machine);
+	addAllBucketsToStackB(
+		numberOfBuckets,
+		aStack.len,
+		buckets,
+		numberOfElements,
+		machine);
+
 	sortStack(&machine->stacks[1]);
 	pushBtoA(machine);
 
 	free(numberOfElements);
 
-	t_element* cursor = machine->stacks[0].first;
+	cursor = machine->stacks[0].first;
 	while (cursor) {
 		printf("%d\n", cursor->val);
 		cursor = cursor->next;

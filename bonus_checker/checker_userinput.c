76e6a8090b9a96b75efd231899c9c13928209bf4
Lexxy <lenivorb@c3a10c3.42berlin.de>
2026-08-04 18:09:25 +0200

    started my work on bonus checker

---
+++ bonus_checker/checker_userinput.c
@@ -0,0 +1,47 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   checker_userinput.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: intranam <intranam@student.42berlin.d      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 20xx/xx/xx xx:xx:xx by intranam          #+#    #+#             */
+/*   Updated: 2026/08/04 17:48:43 by lenivorb         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+// --- include ---
+
+#include "./push_swap.h"
+
+// --- DOC ---
+
+/*
+	... your comment here ...
+*/
+
+// --- define ---
+
+/* TODO: write comment, norminette check */
+
+int	checker_validate_userinput(const char **argv, int argc)
+{
+	int		*arr;
+	int		len;
+
+	if (!(is_args_valid(argv, argc)))
+		return (0);
+	len = count_valid_numbers(argv, argc);
+	if (len == 0)
+		return (0);
+	arr = get_int_list(argv, argc);
+	if (!arr)
+		return (0);
+	if (!(is_numbers_unique(arr, len)))
+	{
+		free(arr);
+		return (0);
+	}
+	free(arr);
+	return (1);
+}
